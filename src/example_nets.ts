99259f4921a0c3ccfb098b5cdd5b327b196a5a6d
DKormann <dominikormann@outlook.com>
2024-11-08 16:41:32 +0100

    examples work

--- src/example_nets.ts
+++ src/example_nets.ts
@@ -5,6 +5,20 @@ const example_nets = [
   [
     "erase",
     "@main=r&(r *)~*"
+  ],
+  [
+    "annihilate",
+    "@main=r&(r *)~(* *)"
+  ],
+  [
+    "commute",
+    "@main=r&(r *)~{* *}"
+  ],
+  [
+    "call",
+    `@main=r&(r *)~@fn
+    @fn={(a a) *}
+    `
   ]
 ]
 
